163349e4e137406b2accd159c053151b0302119d
Carlos Gonzalez <carlosdav1@outlook.com>
2022-04-09 17:51:02 -0600

    Added very basic todo components. Needs styling

---
+++ src/components/ToDoContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='todo-container'>
+        <ToDoItem name='Do laundry' :val=true />
+        <ToDoItem name='Cook dinner'/>
+        <ToDoItem name='Study TS'/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ToDoItem from './ToDoItem.vue';
+
+export default defineComponent({
+    components: {
+        ToDoItem,
+    }
+});
+</script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <span class="todo-item">
+        <input type="checkbox" :id=item :name=item :checked=isChecked>
+        <label :for=item>{{ this.item }}</label>
+    </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+  props: {
+    name: { type:String, required:true},
+    val: Boolean,
+  },
+  data() {
+    return {
+      item: this.name,
+      isChecked: this.val,
+    }
+  }
+})
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.todo-item{
+    width: max-content;
+    
+    & input{
+        margin: 0;
+        margin-right: 0.25rem;
+        width: 1rem;
+        height: 1rem;
+    }
+
+    & label{
+        font-size: 1rem;
+        line-height: 1rem;
+    }
+}
+</style>
