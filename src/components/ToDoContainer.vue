c84eaaeb940c2b912f4f041dffc89bdfbafe22c5
Aaron Fischer <amfischer21@gmail.com>
2022-04-11 15:44:32 -0500

    created TodoItem custom type & separated test data

--- src/components/ToDoContainer.vue
+++ src/components/ToDoContainer.vue
@@ -1,18 +1,31 @@
 <template>
     <div class='todo-container'>
-        <ToDoItem name='Do laundry' :val=true />
-        <ToDoItem name='Cook dinner'/>
-        <ToDoItem name='Study TS'/>
+        <ToDoItem v-for="(item, index) in todoList" :key="index" :name="item.name" v-model="item.completed" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ToDoItem from './ToDoItem.vue';
+import { TodoItemType } from '@/types';
+import testData from '@/assets/seeder.json'
 
 export default defineComponent({
     components: {
         ToDoItem,
+    },
+    data() {
+        return {
+            todoList: testData as TodoItemType[]
+        }
     }
 });
 </script>
+
+<style lang="scss" scoped>
+.todo-container {
+    display: flex;
+    flex-direction: column;
+    margin: 5rem;
+}
+</style>
